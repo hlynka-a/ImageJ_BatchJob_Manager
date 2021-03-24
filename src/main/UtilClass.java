4d5b0a8ca561c926a84b60beec7da5c094ebb7c0
Andrew H <hlynka1@hotmail.com>
2021-03-24 12:54:43 -0400

    2021-03-24 - Implement "returnAverage" function
    Implement "returnAverage" function to return average value of provided array of numbers.

--- src/main/UtilClass.java
+++ src/main/UtilClass.java
@@ -4,10 +4,20 @@ public class UtilClass {
 
 	
 	public double returnAverage(double[] num) {
-		// TODO: Calculate and return correct average of numbers provided as parameters.
 		// Follow "avg" logic as considered standard (refer to Microsoft Excel implementation)
 		
+		double returnValue = 0;
 		
-		return 0;
+		if (num == null || num.length == 0) {
+			
+		} else {
+			double sum = 0;
+			for (int i = 0; i < num.length; i++) {
+				sum += num[i];
+			}
+			returnValue = sum / num.length;
+		}
+		
+		return returnValue;
 	}
 }
