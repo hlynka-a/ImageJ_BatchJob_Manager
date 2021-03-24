27a8f8459cc901f5996e8f96817f0f194085fdaa
Andrew H <hlynka1@hotmail.com>
2021-03-24 12:46:08 -0400

    Create UtilClass.java
    Create UtilClass.java , a class to keep handy functions that will be needed elsewhere in the project.

---
+++ src/main/UtilClass.java
@@ -0,0 +1,13 @@
+package main;
+
+public class UtilClass {
+
+	
+	public double returnAverage(double[] num) {
+		// TODO: Calculate and return correct average of numbers provided as parameters.
+		// Follow "avg" logic as considered standard (refer to Microsoft Excel implementation)
+		
+		
+		return 0;
+	}
+}
